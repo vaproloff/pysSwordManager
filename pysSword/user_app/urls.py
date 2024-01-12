2c73161ee6528e287132f2b363142f980802997f
Konstantin Pivnov <vaproloff@ya.ru>
2024-01-12 17:33:12 +0300

    simplify password reset routes

--- pysSword/user_app/urls.py
+++ pysSword/user_app/urls.py
@@ -1,13 +1,11 @@
+from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, \
+    PasswordResetCompleteView
 from django.urls import path
 from .views import (
     register,
     user_login,
     user_logout,
     user_profile,
-    CustomPasswordResetView,
-    CustomPasswordResetDoneView,
-    CustomPasswordResetConfirmView,
-    CustomPasswordResetCompleteView,
 )
 
 urlpatterns = [
@@ -15,8 +13,16 @@ urlpatterns = [
     path('login/', user_login, name='login'),
     path('logout/', user_logout, name='logout'),
     path('profile/', user_profile, name='profile'),
-    path('password_reset/', CustomPasswordResetView.as_view(), name='password_reset'),
-    path('password_reset/done/', CustomPasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', CustomPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('reset/done/', CustomPasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('password_reset/',
+         PasswordResetView.as_view(template_name='user_app/password_reset_form.html'),
+         name='password_reset'),
+    path('password_reset/done/',
+         PasswordResetDoneView.as_view(template_name='user_app/password_reset_done.html'),
+         name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',
+         PasswordResetConfirmView.as_view(template_name='user_app/password_reset_confirm.html'),
+         name='password_reset_confirm'),
+    path('reset/done/',
+         PasswordResetCompleteView.as_view(template_name='user_app/password_reset_complete.html'),
+         name='password_reset_complete'),
 ]
