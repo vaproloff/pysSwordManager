64718c6314106038634eca83f205be59a9528e90
Konstantin Pivnov <vaproloff@ya.ru>
2024-02-04 01:51:10 +0300

    modified user profile styles

--- pysSword/static/script/user_profile.js
+++ pysSword/static/script/user_profile.js
@@ -1,8 +1,3 @@
-function showEditField(formId, inputId, curValue) {
-    document.getElementById(formId).style.display = 'block';
-    document.getElementById(inputId).value = curValue;
-}
-
 function showChangePasswordForm() {
-    document.getElementById('password-form').style.display = 'block';
+    document.getElementById('profile-password-form').style.display = 'block';
 }
