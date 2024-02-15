b29d78a80d3932a1013391e202b90ca286ef0e41
Konstantin Pivnov <vaproloff@ya.ru>
2024-02-15 15:19:18 +0300

    added password entries app to admin panel

--- pysSword/pass_keeper/admin.py
+++ pysSword/pass_keeper/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import PasswordEntry
+
+
+class PasswordEntryAdmin(admin.ModelAdmin):
+    list_display = ['title', 'user_email', 'date_created', 'last_updated']
+    ordering = ['-last_updated']
+    list_filter = ['date_created', 'last_updated']
+    search_fields = ['title', 'username', 'website', 'user__email']
+    search_help_text = 'Поиск клиентов по полям Наименование, Веб-сайт, Логин, Пользователь'
+    readonly_fields = ['date_created', 'last_updated']
+    fields = ['title', 'website', 'username', 'notes', 'date_created', 'last_updated', 'user']
+    list_per_page = 10
+
+    @staticmethod
+    def user_email(obj):
+        return obj.user.email
+
+
+admin.site.register(PasswordEntry, PasswordEntryAdmin)
